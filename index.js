2ccba0c36625a3d62462aef36f3497b2de127f6e
Nathan <nateyl.b@gmail.com>
2020-02-25 09:18:05 -0800

    processDuplicateFree passed

--- index.js
+++ index.js
@@ -172,8 +172,8 @@ if(bool != true){
  * [2] Invoking `processDuplicateFree` passing `[1,1,2,2,3]` and `(arr) => arr.length`,
  * should return 3.
 */
-function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */) {
-  /* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */
+function processDuplicateFree(list, callback) {
+ return list.filter((item,index) => list.indexOf(item) === index)
 }
 
 /////////////// HIGHER-ORDER ARRAY METHODS ///////////////
